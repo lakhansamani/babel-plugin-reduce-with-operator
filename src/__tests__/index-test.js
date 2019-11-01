63432c0712ae56795b195392bff24ed0e34449a6
lakhansamani <lakhan.m.samani@gmail.com>
2019-11-02 03:30:54 +0530

    chore: add prettier + format code

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -1,39 +1,38 @@
 const babel = require('babel-core');
 const plugin = require('../');
 
-const data = [1,2,3,4,5];
+const data = [1, 2, 3, 4, 5];
 function expectedResult(operator) {
-    switch(operator) {
-        case "+":
-            return data.reduce((a, c) => a + c);
-        case "-":
-            return data.reduce((a, c) => a - c);
-        case "*":
-            return data.reduce((a, c) => a * c);
-        case "/":
-            return data.reduce((a, c) => a / c);
-        case "%":
-            return data.reduce((a, c) => a % c);
-        
-    }
+	switch (operator) {
+		case '+':
+			return data.reduce((a, c) => a + c);
+		case '-':
+			return data.reduce((a, c) => a - c);
+		case '*':
+			return data.reduce((a, c) => a * c);
+		case '/':
+			return data.reduce((a, c) => a / c);
+		case '%':
+			return data.reduce((a, c) => a % c);
+	}
 }
 function getTestData(operator) {
-    return `
+	return `
         const arr = [${data}];
         const res = arr.reduceWithOperator("${operator}");
-    `
+    `;
 }
 
-const operators = ["+", "-", "*", "/", "%"];
+const operators = ['+', '-', '*', '/', '%'];
 
 operators.forEach(item => {
-    it(`test reduceWithOperator with "${item}"`, () => {
-        const result = babel.transform(getTestData(item), {plugins: [plugin]});
-        const f = new Function(`
+	it(`test reduceWithOperator with "${item}"`, () => {
+		const result = babel.transform(getTestData(item), { plugins: [plugin] });
+		const f = new Function(`
           ${result.code};
           return res;
         `);
-        const res = f();
-        expect(res).toBe(expectedResult(item));
-      });
-})
+		const res = f();
+		expect(res).toBe(expectedResult(item));
+	});
+});
--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 module.exports = function(babel) {
-    const { types: t } = babel;
+	const { types: t } = babel;
 
-    return {
-      name: "string-reducer",
-      visitor: {
-        CallExpression(path) {
-            const operators = ["+", "-", "*", "/", "%"];
-          if (path.node.callee.property.name === "reduceWithOperator" && path.node.arguments[0].type === "StringLiteral" && operators.indexOf(path.node.arguments[0].value) > -1) {
-            const identifiers = [t.identifier("a"), t.identifier("c")];
-            const arg = t.arrowFunctionExpression(
-              identifiers,
-              t.binaryExpression(path.node.arguments[0].value, identifiers[0], identifiers[1])
-            );
-            path.node.callee.property.name = 'reduce';
-            path.node.arguments[0] = arg;
-          }
-          return;
-        }
-      }
-    };
-  }
-  
+	return {
+		name: 'string-reducer',
+		visitor: {
+			CallExpression(path) {
+				const operators = ['+', '-', '*', '/', '%'];
+				if (
+					path.node.callee.property.name === 'reduceWithOperator' &&
+					path.node.arguments[0].type === 'StringLiteral' &&
+					operators.indexOf(path.node.arguments[0].value) > -1
+				) {
+					const identifiers = [t.identifier('a'), t.identifier('c')];
+					const arg = t.arrowFunctionExpression(
+						identifiers,
+						t.binaryExpression(
+							path.node.arguments[0].value,
+							identifiers[0],
+							identifiers[1],
+						),
+					);
+					path.node.callee.property.name = 'reduce';
+					path.node.arguments[0] = arg;
+				}
+				return;
+			},
+		},
+	};
+};
